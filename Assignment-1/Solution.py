5bf13ed92ea24ca4b2ac84d7120ed03a8609c187
MurthyVemuri <murthyvemuri211@gmail.com>
2018-07-25 16:48:35 +0530

    pylint warnings are modified.

--- Assignment-1/Solution.py
+++ Assignment-1/Solution.py
@@ -1,24 +1,26 @@
 # Exercise: Assignment-1
-# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
+# Write a Python function, factorial(n), that takes in one number and
+# returns the factorial of given number.
 
 # This function takes in one number and returns one number.
+import math
 
 
 def factorial(n):
-	'''
-	n is positive Integer
+    '''
+    n is positive Integer
 
-	returns: a positive integer, the factorial of n.
-	'''
-	# Your code here
-	
+    returns: a positive integer, the factorial of n.
+    '''
+    # Your code here
+    return math.factorial(n)
 
 
 def main():
-	data=input()
-	for i in range(int(data)):
-		a = input()
-		print(factorial(int(a)))
+    data = input()
+    for i in range(int(data)):
+        a = input()
+        print(factorial(int(a)))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
